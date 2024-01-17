cf5fe1a275671f67e3bc9f03b035daedff88c5c8
Nickolai Grachev <nickbd95@gmail.com>
2024-01-18 00:59:34 +0300

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Nikolai Grachev on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
